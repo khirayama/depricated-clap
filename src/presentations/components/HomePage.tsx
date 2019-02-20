c20018239f6754188519f0f63cb35444ffbf1f15
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-02-20 09:04:10 +0900

    Remove needless interface

--- src/presentations/components/HomePage.tsx
+++ src/presentations/components/HomePage.tsx
@@ -1,8 +1,6 @@
 // tslint:disable:react-a11y-anchors react-no-dangerous-html
 import * as React from 'react';
 
-export interface IProps {}
-
 // tslint:disable-next-line: completed-docs
 export class HomePage extends React.Component {
   // tslint:disable:max-func-body-length
