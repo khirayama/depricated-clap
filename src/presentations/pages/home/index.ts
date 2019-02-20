3faaae7cbb12ffd015c2163f4d61257ce7e7c779
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-02-20 09:05:59 +0900

    Rebuild initialize flow for state

--- src/presentations/pages/home/index.ts
+++ src/presentations/pages/home/index.ts
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { HomePage } from 'presentations/components/HomePage';
+import { HomePageContainer } from 'presentations/containers/HomePageContainer';
+import { IAction, IState } from 'presentations/pages/home/interfaces';
+import { reducer } from 'presentations/pages/home/reducer';
+import { Provider } from 'utils/Container';
+import { Store } from 'utils/Store';
+
+const store: Store<IState, IAction> = new Store(window.state, reducer);
+store.dispatch({
+  actionType: 'INIT',
+  payload: null,
+});
 
 window.addEventListener('DOMContentLoaded', () => {
   const el: HTMLElement = window.document.querySelector('.application');
-  ReactDOM.render(React.createElement(HomePage, window.state), el);
+  ReactDOM.render(React.createElement(Provider, { store }, React.createElement(HomePageContainer)), el);
 });
--- src/presentations/pages/home/reducer.ts
+++ src/presentations/pages/home/reducer.ts
@@ -1,9 +1,22 @@
+import * as uuid from 'uuid/v4';
+
 import { actionTypes } from 'presentations/pages/home/actionTypes';
 import { IAction, IState } from 'presentations/pages/home/interfaces';
+import { Document, IDocument } from 'utils/document/Document';
+
+let doc: Document | null = null;
 
 export function reducer(state: IState, action: IAction): IState {
   const newState: IState = JSON.parse(JSON.stringify(state));
   const payload: {} = action.payload;
+
+  switch (action.actionType) {
+    case 'INIT': {
+      doc = new Document(state.doc);
+      break;
+    }
+    default:
+  }
 
   return newState;
 }
