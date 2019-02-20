13f636a5ca54c40afaa4e9d288af9e3883f640ab
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-02-20 09:11:00 +0900

    Update HomePageContainer

--- src/presentations/containers/HomePageContainer.tsx
+++ src/presentations/containers/HomePageContainer.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 
-import { HomePage } from 'presentations/components/HomePage';
 import { connect, IContainerProps } from 'utils/Container';
+import { IDocument } from 'utils/document/Document';
+import { IItem } from 'utils/document/Item';
+
+export class Paper extends React.Component<{ doc?: IDocument }> {
+  public render(): JSX.Element {
+    const doc: IDocument = this.props.doc;
+
+    return (
+      <div className="Paper">
+        <h1>{doc.title}</h1>
+      </div>
+    );
+  }
+}
 
 // tslint:disable-next-line: variable-name
 export const HomePageContainer: React.ComponentClass = connect(
   class extends React.Component<IContainerProps> {
-    constructor(props: IContainerProps) {
-      super(props);
-    }
-
     public render(): JSX.Element {
-      return <HomePage />;
+      return <Paper doc={this.props.doc} />;
     }
   },
 );
